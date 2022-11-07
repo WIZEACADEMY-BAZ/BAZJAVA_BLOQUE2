640fbfe61f5fee1a127cbd6c83dc1ad553d69aba
DAVID HINOSTROZA LOERA <dhinostroza@ad.elektra.com.mx>
2022-11-07 09:13:43 -0600

    Se crea rama de entregable 2

---
+++ David_Hinostroza_Loera/Entregable 2/entregabledavid/src/main/java/com/wizeline/entregabledavid/model/XmlBean.java
@@ -0,0 +1,14 @@
+/**package com.wizeline.entregabledavid.model;
+
+public class XmlBean {
+    private Object data;
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object object) {
+        this.data = object;
+    }
+
+}**/
