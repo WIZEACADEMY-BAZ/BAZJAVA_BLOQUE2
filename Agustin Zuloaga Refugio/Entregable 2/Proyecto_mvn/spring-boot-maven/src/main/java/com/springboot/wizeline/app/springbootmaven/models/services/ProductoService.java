12637d46db17f74c30ba6463355c745b3a442a6c
azuloagar <agustin.zuloaga@elektra.com.mx>
2022-11-07 13:30:48 -0600

    Spring MVC con un endpoint de cada tipo (GET, POST, PUT y DELETE

---
+++ Agustin Zuloaga Refugio/Entregable 2/Proyecto_mvn/spring-boot-maven/src/main/java/com/springboot/wizeline/app/springbootmaven/models/services/ProductoService.java
@@ -0,0 +1,29 @@
+package com.springboot.wizeline.app.springbootmaven.models.services;
+
+import com.springboot.wizeline.app.springbootmaven.documents.Categoria;
+import com.springboot.wizeline.app.springbootmaven.documents.Producto;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface ProductoService {
+	
+	public Flux<Producto> findAll();
+	
+	public Flux<Producto> findAllConNombreUpperCase();
+	
+	public Flux<Producto> findAllConNombreUpperCaseRepeat();
+	
+	public Mono<Producto> findById(String id);
+	
+	public Mono<Producto> save(Producto producto);
+	
+	public Mono<Void> delete(Producto producto);
+	
+	public Flux<Categoria> findAllCategoria();
+	
+	public Mono<Categoria> findCategoriaById(String id);
+	
+	public Mono<Categoria> saveCategoria(Categoria categoria);
+
+}
