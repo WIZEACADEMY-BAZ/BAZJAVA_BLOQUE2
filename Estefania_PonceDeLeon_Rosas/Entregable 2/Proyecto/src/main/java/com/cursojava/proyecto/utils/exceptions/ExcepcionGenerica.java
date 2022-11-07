4483f382aff76626218cdb3fd980f41c4841ba58
eponceelektra <estefania.ponceleon@elektra.com.mx>
2022-11-07 15:27:03 -0600

    Limpieza

--- Estefania_PonceDeLeon_Rosas/Entregable 2/Proyecto/src/main/java/com/cursojava/proyecto/utils/exceptions/ExcepcionGenerica.java
+++
@@ -1,7 +0,0 @@
-package com.cursojava.proyecto.utils.exceptions;
-
-public class ExcepcionGenerica extends RuntimeException {
-    public ExcepcionGenerica(String mensajeError) {
-        super(mensajeError);
-    }
-}
