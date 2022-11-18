b7c8a8dbf580f3c8afead857c475dd20995ff138
OSWALDO SANTIAGO SANJUAN <osantiago@ad.elektra.com.mx>
2022-11-18 17:25:37 -0600

    Cambios

--- Oswaldo_Santiago_Sanjuan/Entregable2/ProyectoSpringBoot/learningjavamaven/src/main/java/com/wizeline/maven/learningjavamaven/patronesDiseño/Singleton/Client.java
+++ Oswaldo_Santiago_Sanjuan/Entregable2/ProyectoSpringBoot/learningjavamaven/src/main/java/com/wizeline/maven/learningjavamaven/patronesDiseño/Singleton/Client.java
@@ -1,2 +1,12 @@
-package com.wizeline.maven.learningjavamaven.patronesDiseño.Singleton;public class Client {
+package com.wizeline.maven.learningjavamaven.patronesDiseño.Singleton;
+
+public class Client {
+    /*
+    public static void main(String[] args) {
+        DatabaseConfig databaseConfig = DatabaseConfig.getInstance("MySQL");
+        databaseConfig.connect();
+        DatabaseConfig databaseConfig2 = DatabaseConfig.getInstance("Oracle");
+        databaseConfig2.connect();
+    }
+*/
 }
--- Oswaldo_Santiago_Sanjuan/Entregable2/ProyectoSpringBoot/learningjavamaven/src/main/java/com/wizeline/maven/learningjavamaven/patronesDiseño/Singleton/DatabaseConfig.java
+++ Oswaldo_Santiago_Sanjuan/Entregable2/ProyectoSpringBoot/learningjavamaven/src/main/java/com/wizeline/maven/learningjavamaven/patronesDiseño/Singleton/DatabaseConfig.java
@@ -1,2 +1,20 @@
-package com.wizeline.maven.learningjavamaven.patronesDiseño.Singleton;public class DatabaseConfig {
+package com.wizeline.maven.learningjavamaven.patronesDiseño.Singleton;
+
+public class DatabaseConfig {
+    private static  DatabaseConfig instance;
+    public String value;
+
+    private DatabaseConfig(String value){
+        this.value = value;
+    }
+
+    public static DatabaseConfig getInstance(String value){
+        if (instance == null){
+            instance = new DatabaseConfig(value);
+        }
+        return instance;
+    }
+    public void connect(){
+        System.out.println("Connecting to " + value + " database ...");
+    }
 }
