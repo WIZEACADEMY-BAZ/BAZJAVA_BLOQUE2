dc1170329184a1f11c241f5d1ab906b7752fd50b
GUSTAVO SOLAR GAONA <gustavo.solar@elektra.com.mx>
2022-11-24 14:32:16 -0600

    se corrige propiedad en yaml para levantar spring bash sin error

--- Gustavo_solar_gaona/Entregable_3/LearningJavaMaven/src/main/java/com/wizeline/maven/learningjavamaven/configuration/BankAccountJob.java
+++ Gustavo_solar_gaona/Entregable_3/LearningJavaMaven/src/main/java/com/wizeline/maven/learningjavamaven/configuration/BankAccountJob.java
@@ -18,7 +18,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.FileSystemResource;
-import org.springframework.core.io.PathResource;
 
 @Configuration
 public class BankAccountJob {
